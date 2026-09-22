5f5e05bc986287c48b5e643859f6af6593976663
QJiang <jq23010134@163.com>
2026-09-22 19:02:25 +0800

    add sample average program

---
+++ 01_cpp_basics/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main()
+{
+    int sample = 0;
+    int count = 0;
+    int sum = 0;
+
+    std::cout << "请输入整数，按 Ctrl+D 结束输入：\n";
+
+    while (std::cin >> sample)
+    {
+        if (sample < 0 || sample > 100)
+        {
+            continue;
+        }
+
+        sum += sample;
+        ++count;
+    }
+
+    if (count == 0)
+    {
+        std::cout << "没有有效数据。\n";
+    }
+    else
+    {
+        double average = static_cast<double>(sum) / count;
+
+        std::cout << "有效数据个数：" << count << '\n';
+        std::cout << "平均值：" << average << '\n';
+    }
+
+    return 0;
+}
